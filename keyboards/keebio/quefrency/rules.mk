9791191c37a7200d48532ce809d5d70418f3e794
Karim Mansour <karim@mansour.id>
2024-04-29 14:34:39 +0200

    Revert back to Rev1

--- keyboards/keebio/quefrency/rules.mk
+++ keyboards/keebio/quefrency/rules.mk
@@ -15,5 +15,5 @@ SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 
 SPLIT_KEYBOARD = yes
 
-DEFAULT_FOLDER = keebio/quefrency/rev4
+DEFAULT_FOLDER = keebio/quefrency/rev1
 LTO_ENABLE = yes
